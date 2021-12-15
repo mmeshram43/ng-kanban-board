2d863f0fe0468e6a96102c3c78ced01fef050448
Mayur Meshram <mmeshram43@gmail.com>
2021-12-15 08:44:13 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,55 @@
 import { Component } from '@angular/core';
-
+import { Task } from './task/task';
+import { CdkDragDrop, transferArrayItem } from '@angular/cdk/drag-drop';
+import { MatDialog } from "@angular/material/dialog";
+import { TaskDialogComponent, TaskDialogResult } from './task-dialog/task-dialog.component';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ng-kanban';
+
+
+  constructor( private dialog : MatDialog) {
+    
+  }
+
+  title = 'kanban-fire';
+  todo: Task[] = [
+  ];
+
+  inProgress: Task[] = [];
+  done: Task[] = [];
+
+newTask() : void{
+  const dialogRef = this.dialog.open(TaskDialogComponent, {
+    width : '270px',
+    data : {
+      task : {}
+    },
+  });
+  dialogRef
+  .afterClosed()
+  .subscribe((result : TaskDialogResult) => this.todo.push(result.task) );
+}
+
+
+  editTask(list: string, task: Task): void { }
+
+  drop(event: CdkDragDrop<Task[] | any>): void {
+    if (event.previousContainer === event.container) {
+      return;
+    }
+    if (!event.container.data || !event.previousContainer.data) {
+      return;
+    }
+    transferArrayItem(
+      event.previousContainer.data,
+      event.container.data,
+      event.previousIndex,
+      event.currentIndex
+    );
+  }
+
 }
